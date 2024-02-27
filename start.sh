95aefec23d26d4e5bcc71be6b800e11ba8dd3903
minu1215 <mkw3295@gmail.com>
2024-02-27 16:11:09 +0900

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BUILD_ID=dontKillMe nohup java -jar -Dserver.port=8888 ~/app/hello-rest-sb/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/hello-rest-sb/log.log &
+nohup java -jar -Dserver.port=8888 ~/app/hello-rest-sb/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/hello-rest-sb/log.log &
 echo $! > ~/app/hello-rest-sb/pid.file
